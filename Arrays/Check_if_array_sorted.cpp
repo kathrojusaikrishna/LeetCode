0422a7622cbb9c2a142b69c50fdfba8e616fe48d
SAIKRISHNA KATHROJU <saikrishnakathroju@SAIKRISHNAs-MacBook-Air.local>
2026-02-12 00:41:04 +0530

    Added check if array is sorted and rotated (easy)

---
+++ Arrays/Check_if_array_sorted.cpp
@@ -0,0 +1,23 @@
+// Problem: Check if array is sorted and rotated
+// Difficulty: Easy
+// Approach: simple iteration
+// Time: O(n)
+// Space: O(1)
+
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int n = nums.size();
+        int counter=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n])counter++;
+        }
+
+        if(counter <=1)return true;
+        return false;
+    }
+};
