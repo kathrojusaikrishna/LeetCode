f13dec9a06a673a87c58da44a1e7ee5172d81990
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-05-25 00:53:40 +0530

    Added Minstack (Medium)

---
+++ Stack/minStack.cpp
@@ -0,0 +1,42 @@
+// Problem: Minstack building
+// Difficulty: Medium
+// Approach: using stacks
+// Time: O(1)
+// Space: O(n)
+
+
+class MinStack {
+public:
+    stack<int>st;
+    stack<int>minStack;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minStack.empty() || minStack.top() >= val)minStack.push(val);
+    }
+    
+    void pop() {
+
+        if(st.top()==minStack.top())minStack.pop();
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
