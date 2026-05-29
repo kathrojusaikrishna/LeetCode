f199afb4a890ba7f51391137a6974af8f7ad8377
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-05-29 16:21:24 +0530

    Added Bottom view (Medium)

---
+++ Tree/bottomView.cpp
@@ -0,0 +1,43 @@
+// Problem: Bottom view
+// Difficulty: Medium
+//platform: Geeksforgeeks
+// Approach: using queue
+// Time: O(n)
+// Space: O(h) -> for queue
+
+
+class Solution {
+  public:
+    vector<int> bottomView(Node *root) {
+        // code here
+        
+        vector<int>ans;
+        if(!root)return ans;
+        
+        map<int,int>mp;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        
+        while(!q.empty()){
+            
+            int size = q.size();
+            
+            for(int i=0;i<size;i++){
+                
+                auto [temp,vertical] = q.front();
+                q.pop();
+                
+                mp[vertical] = temp->data;
+                
+                if(temp->left)q.push({temp->left,vertical-1});
+                if(temp->right)q.push({temp->right,vertical+1});
+            }
+        }
+        
+        for(auto &it : mp){
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+};
