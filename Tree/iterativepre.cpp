5e7fdfc7c17acdca3bd2e5781e422736bc92e593
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-06-02 15:28:26 +0530

    Added Iterative pre order using stack (Easy)

---
+++ Tree/iterativepre.cpp
@@ -0,0 +1,32 @@
+// Problem: Iterative Pre order
+// Difficulty: Easy
+//platform: Geeksforgeeks
+// Approach: using stack
+// Time: O(n)
+// Space: O(n) -> for stack
+
+class Solution {
+  public:
+    vector<int> preOrder(Node* root) {
+        // code here
+        
+        vector<int>ans;
+        if(!root)return ans;
+        
+        stack<Node*>st;
+        st.push(root);
+        
+        while(!st.empty()){
+            
+            Node* temp = st.top();
+            st.pop();
+            
+            ans.push_back(temp->data);
+            
+            if(temp->right)st.push(temp->right);
+            if(temp->left)st.push(temp->left);
+        }
+        
+        return ans;
+    }
+};
