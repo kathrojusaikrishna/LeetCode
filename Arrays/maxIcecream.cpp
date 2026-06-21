a7acc64f9c5ddc55c12b786c168c2968a1c0ec66
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-06-21 23:16:16 +0530

    Maximum Ice Cream Bars (Medium)

---
+++ Arrays/maxIcecream.cpp
@@ -0,0 +1,24 @@
+// Problem: Maximum Ice Cream Bars
+// Difficulty: Medium
+// Approach: sorting
+// Time: O(nlog n)
+// Space: O(1)
+
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        
+        sort(costs.begin(),costs.end());
+        int n = costs.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(costs[i]<=coins){
+                ans++;
+                coins -= costs[i];
+            }
+        }
+
+        return ans;
+    }
+};
