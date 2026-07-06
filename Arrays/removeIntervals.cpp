f29680b730945b123389697c32e8e2fbd601f1d1
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-07-06 14:39:05 +0530

    Added Remove Covered intervals (Medium)

---
+++ Arrays/removeIntervals.cpp
@@ -0,0 +1,33 @@
+// Problem: Remove Covered intervals
+// Difficulty: Medium
+// Approach: sort and check condition
+// Time: O(nlog n)
+// Space: O(1)
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+        int n = intervals.size();
+        int counter=0;
+        sort(intervals.begin(),intervals.end(), [](const vector<int>&a, const vector<int>&b){
+            if(a[0]==b[0]){
+                return a[1]>b[1];
+            }
+            return a[0]<b[0];
+        });
+
+        int maxVal = intervals[0][1];
+
+        for(int i=1;i<n;i++){
+            if(intervals[i][1]<=maxVal){
+                counter++;
+            }else{
+                maxVal=intervals[i][1];
+            }
+
+        }
+
+        return n-counter;
+    }
+};
