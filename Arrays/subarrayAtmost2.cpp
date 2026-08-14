f4bc0bdde32cddc2bebc0c671d903aa549218e2a
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-08-14 17:30:58 +0530

    Added maximum length of subarray with atmost 2 occurences of each character (Easy)

---
+++ Arrays/subarrayAtmost2.cpp
@@ -0,0 +1,30 @@
+// Problem: maximum length of subarray with atmost 2 occurences of each character
+// Difficulty: Easy
+// Platform: Leetcode
+// Approach: sliding window
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        
+        vector<int>a(26,0);
+
+        int left=0;
+        int len=0;
+
+        for(int right=0;right<s.size();right++){
+            a[s[right]-'a']++;
+
+            while(a[s[right]-'a']>2){
+                a[s[left]-'a']--;
+                left++;
+            }
+
+            len = max(len,right-left+1);
+        }
+
+        return len;
+    }
+};
