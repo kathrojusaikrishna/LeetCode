b5e4c084680d6ce1c87e9c9ae4a824b9eac2c5a0
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-08-21 23:56:14 +0530

    Added Cells with odd values in a matrix (Easy)

---
+++ Arrays/oddValues.cpp
@@ -0,0 +1,34 @@
+// Problem: Cells with odd values in a matrix
+// Difficulty: easy
+// Platform: Leetcode
+// Approach: iteration
+// Time: O(n^2)
+// Space: O(n)
+
+
+class Solution {
+public:
+    int oddCells(int m, int n, vector<vector<int>>& indices) {
+         vector<int> row(m, 0);
+        vector<int> col(n, 0);
+
+        
+        for (auto &x : indices) {
+            row[x[0]]++;
+            col[x[1]]++;
+        }
+
+        int ans = 0;
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((row[i] + col[j]) % 2 == 1) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
