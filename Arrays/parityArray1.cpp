9ca9b2e16734087e38a3f371aa4e7f89256c2f90
Kathroju Sai Krishna <kathsaikrishnaper@gmail.com>
2026-09-02 21:05:14 +0530

    Added Construct Uniform parity I (Easy)

---
+++ Arrays/parityArray1.cpp
@@ -0,0 +1,13 @@
+// Problem: Construct Uniform parity I
+// Difficulty: easy
+// Platform: Leetcode
+// Approach: it will always be possible to make all odd if atleast one element in the array as odd, else all even
+// Time: O(1)
+// Space: O(1)
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
